70d67c1354f913067180d12d7db9772c12ac6cc7
NMMastro <natemastronardi@gmail.com>
2022-07-22 15:21:01 -0400

    Finished Checker Board

--- src/index.js
+++ src/index.js
@@ -1,17 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Piece extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+class Square extends React.Component {
+  render() {
+    return(
+      <button className={"square " + this.props.color}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class GameBoard extends React.Component {
+
+  renderSquare(i, color) {
+    return <Square value={i} color={color}/>
+  }
+
+  render() {
+    return(
+      <div className="board centered">
+        <div className="board-row">
+          {this.renderSquare(1, "light-square")}
+          {this.renderSquare(2, "dark-square")}
+          {this.renderSquare(3, "light-square")}
+          {this.renderSquare(4, "dark-square")}
+          {this.renderSquare(5, "light-square")}
+          {this.renderSquare(6, "dark-square")}
+          {this.renderSquare(7, "light-square")}
+          {this.renderSquare(8, "dark-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(9, "dark-square")}
+          {this.renderSquare(10, "light-square")}
+          {this.renderSquare(11, "dark-square")}
+          {this.renderSquare(12, "light-square")}
+          {this.renderSquare(13, "dark-square")}
+          {this.renderSquare(14, "light-square")}
+          {this.renderSquare(15, "dark-square")}
+          {this.renderSquare(16, "light-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(1, "light-square")}
+          {this.renderSquare(2, "dark-square")}
+          {this.renderSquare(3, "light-square")}
+          {this.renderSquare(4, "dark-square")}
+          {this.renderSquare(5, "light-square")}
+          {this.renderSquare(6, "dark-square")}
+          {this.renderSquare(7, "light-square")}
+          {this.renderSquare(8, "dark-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(9, "dark-square")}
+          {this.renderSquare(10, "light-square")}
+          {this.renderSquare(11, "dark-square")}
+          {this.renderSquare(12, "light-square")}
+          {this.renderSquare(13, "dark-square")}
+          {this.renderSquare(14, "light-square")}
+          {this.renderSquare(15, "dark-square")}
+          {this.renderSquare(16, "light-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(1, "light-square")}
+          {this.renderSquare(2, "dark-square")}
+          {this.renderSquare(3, "light-square")}
+          {this.renderSquare(4, "dark-square")}
+          {this.renderSquare(5, "light-square")}
+          {this.renderSquare(6, "dark-square")}
+          {this.renderSquare(7, "light-square")}
+          {this.renderSquare(8, "dark-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(9, "dark-square")}
+          {this.renderSquare(10, "light-square")}
+          {this.renderSquare(11, "dark-square")}
+          {this.renderSquare(12, "light-square")}
+          {this.renderSquare(13, "dark-square")}
+          {this.renderSquare(14, "light-square")}
+          {this.renderSquare(15, "dark-square")}
+          {this.renderSquare(16, "light-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(1, "light-square")}
+          {this.renderSquare(2, "dark-square")}
+          {this.renderSquare(3, "light-square")}
+          {this.renderSquare(4, "dark-square")}
+          {this.renderSquare(5, "light-square")}
+          {this.renderSquare(6, "dark-square")}
+          {this.renderSquare(7, "light-square")}
+          {this.renderSquare(8, "dark-square")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(9, "dark-square")}
+          {this.renderSquare(10, "light-square")}
+          {this.renderSquare(11, "dark-square")}
+          {this.renderSquare(12, "light-square")}
+          {this.renderSquare(13, "dark-square")}
+          {this.renderSquare(14, "light-square")}
+          {this.renderSquare(15, "dark-square")}
+          {this.renderSquare(16, "light-square")}
+        </div>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GameBoard />)
